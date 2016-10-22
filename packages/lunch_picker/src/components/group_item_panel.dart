f1000cfdf4f0ae8fe69b7f4d267a0b8c8528d51a
Dominic Frost <frostationdevice@gmail.com>
2016-10-21 19:29:19 -0600

    build commit

---
+++ packages/lunch_picker/src/components/group_item_panel.dart
@@ -0,0 +1,54 @@
+part of lunch_picker;
+
+var GroupItemPanel = react.registerComponent(() => new _GroupItemPanel());
+
+class _GroupItemPanel extends flux.FluxComponent<PickerActions, PickerStore> {
+  String get groupItemValue => state['groupItemValue'];
+
+  Map getInitialState() => {'groupItemValue': ''};
+
+  bool get buttonDisabled => !store.canCreateMember && groupItemValue == '';
+
+  bool get inputDisabled => !store.canCreateMember;
+
+  componentDidMount() {
+    store.listen((_) => redraw());
+  }
+
+  render() {
+    return react.div({
+      'className': 'panel panel-default'
+    }, [
+      react.div({'className': 'panel-heading'}, 'Group Items'),
+      react.div(
+          {'className': 'panel-body'},
+          react.form({
+            'role': 'form'
+          }, [
+            react.input({
+              'className': 'form-group form-control',
+              'type': 'text',
+              'value': groupItemValue,
+              'disabled': inputDisabled,
+              'onChange': _onChange,
+              'placeholder': 'Insert group item ...'
+            }),
+            react.button({
+              'className': 'form-group btn btn-md btn-primary',
+              'type': 'button',
+              'disabled': buttonDisabled,
+              'onClick': _onClick
+            }, 'Insert')
+          ]))
+    ]);
+  }
+
+  _onChange(react.SyntheticFormEvent event) {
+    setState({'groupItemValue': event.target.value});
+  }
+
+  _onClick(_) {
+    setState({'groupItemValue': ''});
+    actions.createGroupItem(groupItemValue);
+  }
+}
---
+++ packages/lunch_picker/src/models/group_item.dart
@@ -0,0 +1,26 @@
+part of lunch_picker;
+
+class GroupItem {
+  String name;
+  Group group;
+  int lastPicked;
+  bool hasPicked;
+
+  GroupItem(this.name, this.group, {this.lastPicked: 0, this.hasPicked: false});
+
+  GroupItem.fromJson(Map json) {
+    this.name = json['name'];
+    this.group = new Group.fromJson(json['group']);
+    this.lastPicked = json['lastPicked'];
+    this.hasPicked = json['hasPicked'];
+  }
+
+  Map toJson() {
+    return {
+      'name': this.name,
+      'group': this.group.toJson(),
+      'lastPicked': this.lastPicked,
+      'hasPicked': this.hasPicked,
+    };
+  }
+}
