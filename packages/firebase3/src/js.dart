03450f0e6fea8bcaace5c22f1a78a74692bdd925
Dominic Frost <frostationdevice@gmail.com>
2016-10-21 19:33:05 -0600

    build commit

--- packages/firebase3/src/js.dart
+++
@@ -1,9 +0,0 @@
-/// This class is a wrapper for the jsObject. All the specific JsObject
-/// wrappers extend from it.
-abstract class JsObjectWrapper<T> {
-  /// JS object.
-  T jsObject;
-
-  /// Creates a new JsObjectWrapper type from a [jsObject].
-  JsObjectWrapper.fromJsObject(this.jsObject);
-}
